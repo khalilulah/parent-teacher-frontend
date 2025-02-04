a07a9d40ef55ad9c6065c3fe42e08f95be7d2f6a
Alausa <alausaabdulazeez@gmail.com>
2025-02-04 11:04:54 +0100

    first version of file upload works

--- utils/utilFunctions.js
+++ utils/utilFunctions.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://192.168.89.56:8080/api";
-export const SOCKET_BASE_URL = "http://192.168.89.56:8080";
+export const BASE_URL = "http://192.168.45.56:8080/api";
+export const SOCKET_BASE_URL = "http://192.168.45.56:8080";
